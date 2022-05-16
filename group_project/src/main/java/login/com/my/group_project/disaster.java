1246b4cdb1358f247eaa0cc8f82d515c24d20c5d
Cava <89451089+AchievementCava@users.noreply.github.com>
2022-05-17 02:46:38 +0800

    added swing classes for disaster information part, added text files to integrate

---
+++ group_project/src/main/java/login/com/my/group_project/disaster.java
@@ -0,0 +1,16 @@
+package login.com.my.group_project;
+
+import java.awt.Toolkit.*;
+import java.awt.event.*;
+
+public class disaster {
+    public void openWindow(String country) {
+        
+        switch (country) {
+            
+            case "x":
+                
+        }
+        
+    }
+}
