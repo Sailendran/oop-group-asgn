4b566c4f7516571c792b12bbb6daf27664f7b77f
Cava <89451089+AchievementCava@users.noreply.github.com>
2022-05-22 12:19:04 +0800

    Added login and signup functionality

---
+++ group_project/src/main/java/com/my/group_project/Accounts.java
@@ -0,0 +1,170 @@
+package com.my.group_project;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+class Accounts {
+    
+    //testing
+    public static void main(String[] args) {
+        //System.out.println(kaiserCypher("h0m0sexual".toCharArray()));
+    }
+    
+    //universal shift value 
+    static int shift = 8;    
+    
+    // Encrypts text using shift
+    public static String kaiserCypher(char[] chars) {
+        
+        char[] ciphertext = new char[chars.length];
+        int i = 0;
+        
+        for (char x : chars) {
+            
+            char y = (char)(x + Accounts.shift);
+            ciphertext[i] = y;
+            i++;
+            
+        }
+        
+        return new String(ciphertext);
+    }
+ 
+    // Decrypts cipher using shift
+    //NOTICE: MAY BE UNUSED (ENCRYPT GIVEN PASSWORD TO CHECK FOR EQUIVALANCE
+    public static String kaiserDecypher(String ciphertext) {
+        
+        char[] chars = ciphertext.toCharArray();
+        char[] text = new char[ciphertext.length()];
+        int i = 0;
+        
+        for (char x : chars) {
+            
+            char y = (char)(x - Accounts.shift);
+            text[i] = y;
+            i++;
+            
+        }
+        
+        
+        return new String(text);
+    }
+    
+    //login and signup handling
+    public static boolean login(String username, char[] password) {
+        
+        boolean accountExists = false;
+        boolean passwordMatch = false;
+
+        JSONParser jp = new JSONParser();
+        
+        //reading JSON to get language TODO add colour mode later
+        try {
+            
+                Object obj = jp.parse(new FileReader("accounts.json"));
+                JSONObject jo = (JSONObject) obj;
+                Set<String> accountusernames = jo.keySet();
+                
+                //testing
+                for (String x : accountusernames) {
+                    
+                    if (username.equals(x)) {
+                        accountExists = true;
+                        break;
+                    }
+                
+                }
+                
+                if (accountExists) {
+                    String encryptPassInput = kaiserCypher(password);
+                    
+                    if (encryptPassInput.equals(jo.get(username))) {
+                        passwordMatch = true;
+                    } else {
+                    }
+                    
+                }
+                
+                if (accountExists && passwordMatch) {
+                    MainMenu mm = new MainMenu();
+                    mm.setVisible(true);
+                }
+                
+                return (accountExists && passwordMatch);
+
+                
+        } catch (FileNotFoundException e) {
+            
+            System.out.println("The file wasn't found");
+            
+        } catch (IOException e) {
+            
+            System.out.println("There was an IO Exception");
+            
+        } catch (ParseException e) {
+            
+            System.out.println("There was an error parsing JSON data");
+        }
+       
+    return false;
+    }
+    
+    
+    
+    public static boolean signup(String username, char[] password, char[] confirm) throws ParseException {
+        
+        boolean accountExists = false;
+        
+        if (MyUtils.arraysEqual(password, confirm)) {
+            
+            JSONParser jp = new JSONParser();
+            Object obj = null;
+            try {
+                obj = jp.parse(new FileReader("accounts.json"));
+            } catch (IOException ex) {
+                Logger.getLogger(Accounts.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("IO Exception");
+                return false;
+            }
+            JSONObject jo = (JSONObject)obj;
+            Set<String> accountusernames = jo.keySet();
+            for (String x : accountusernames) {
+                
+                if (username.equals(x)) {
+                    accountExists = true;
+                    break;
+                }
+                
+            }
+            if (!accountExists) {
+                //add the account to the JSON
+                jo.put(username, kaiserCypher(password));
+                
+                try (FileWriter file = new FileWriter("accounts.json")) {
+                    file.write(jo.toJSONString());
+                    Login lg = new Login();
+                    lg.setVisible(true);
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(Accounts.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("Couldn't write account");
+                }
+                
+            }
+        return true;    
+        }
+            
+    return false;        
+    }
+    
+
+}
---
+++ group_project/src/main/java/com/my/group_project/MyUtils.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.my.group_project;
+
+public class MyUtils {
+    
+    public static boolean arraysEqual(char[] a, char[] b) {
+        
+        if (a.length == b.length) {
+            
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {
+                    return false;
+                }
+            } 
+            
+            return true;
+            
+        } else {
+            return false;
+        }
+        
+    }
+    
+}
