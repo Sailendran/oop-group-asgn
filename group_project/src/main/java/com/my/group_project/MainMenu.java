98bc9cffadc48e77ff231172916dfd6c868fe49c
Cava <89451089+AchievementCava@users.noreply.github.com>
2022-06-04 18:27:18 +0800

    Fixed survey menu design

--- group_project/src/main/java/com/my/group_project/MainMenu.java
+++ group_project/src/main/java/com/my/group_project/MainMenu.java
@@ -55,6 +55,11 @@ public class MainMenu extends javax.swing.JFrame {
         });
 
         jButton3.setText("Quiz on disasters");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Settings...");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -119,6 +124,12 @@ public class MainMenu extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        Survey sv = new Survey();
+        sv.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
