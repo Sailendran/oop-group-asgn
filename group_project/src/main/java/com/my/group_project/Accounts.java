faf6f42c2870c1ef2d3b1315f8aac8d6905da107
Cava <89451089+AchievementCava@users.noreply.github.com>
2022-06-04 17:18:16 +0800

    Fixed dependencies issues, file now runs but translation module does not

--- group_project/src/main/java/com/my/group_project/Accounts.java
+++ group_project/src/main/java/com/my/group_project/Accounts.java
@@ -4,10 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//json-simple
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
